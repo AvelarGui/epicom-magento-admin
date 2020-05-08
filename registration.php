767411255010413538538e998ba1f34775aca79c
Guilherme Avelar <guilhermesantosavelar@gmail.com>
2020-05-07 23:20:44 -0300

    Epicom admin theme

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Dangs_Admintheme',
+    'Epicom_Admintheme',
     __DIR__
 );
